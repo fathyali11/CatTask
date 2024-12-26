f909cbbdf398b9baee9371f5be3d8f67fca278fd
fathy <fathy@FATHYALI>
2024-12-26 03:24:51 +0200

    add mapping for todo request,response

---
+++ CatTask.Domain/Mappings/ToDoMappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CatTask.Domain.DTO.TODos;
+using CatTask.Domain.Entities;
+
+namespace CatTask.Domain.Mappings;
+public class ToDoMappings:Profile
+{
+    public ToDoMappings()
+    {
+        CreateMap<ToDoRequest,ToDo>()
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+
+        CreateMap<ToDo, ToDoResponse>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status));
+    }
+}
