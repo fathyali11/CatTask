e807eb79193ef60a069a9d3ef3086d8586e24b0f
fathy <fathy@FATHYALI>
2024-12-26 03:25:09 +0200

    add validation on todo request

---
+++ CatTask.Domain/FluentValidations/ToDoValidations/ToDoRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using CatTask.Domain.Abstractions;
+using CatTask.Domain.DTO.TODos;
+using FluentValidation;
+
+namespace CatTask.Domain.FluentValidations.ToDoValidations;
+public class ToDoRequestValidator:AbstractValidator<ToDoRequest>
+{
+    public ToDoRequestValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required");
+
+        RuleFor(x => x.Status)
+            .Must(ValidStatus)
+            .WithMessage($"{nameof(ToDoRequest.Status)} is invalid it should be {ConstValues.pending} or {ConstValues.completed}");
+    }
+    private bool ValidStatus(string status)
+    {
+        return string.Equals(status, ConstValues.pending,StringComparison.OrdinalIgnoreCase) ||
+               string.Equals(status, ConstValues.completed, StringComparison.OrdinalIgnoreCase);
+    }
+}
