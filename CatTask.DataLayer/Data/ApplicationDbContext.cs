c13a2edc8a84acf8aefc755bc2d9bba7cb0da696
fathy <fathy@FATHYALI>
2024-12-26 02:51:22 +0200

    add applicationDbContext and inject it

---
+++ CatTask.DataLayer/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using CatTask.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatTask.DataLayer.Data;
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<ToDo> ToDos { get; set; } = default!;
+}
--- CatTask.Web/Program.cs
+++ CatTask.Web/Program.cs
@@ -1,3 +1,6 @@
+using CatTask.DataLayer.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")??
+    throw new InvalidDataException("Connection string is not found");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
