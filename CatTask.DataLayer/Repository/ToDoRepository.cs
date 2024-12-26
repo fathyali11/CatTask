3ab4ea9811c23a0aea295423cecc738d1d65b74e
fathy <fathy@FATHYALI>
2024-12-26 03:10:52 +0200

    add update and its implementation

---
+++ CatTask.DataLayer/Repository/ToDoRepository.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CatTask.DataLayer.Data;
+using CatTask.Domain.DTO.TODos;
+using CatTask.Domain.Entities;
+using CatTask.Domain.IRepository;
+
+namespace CatTask.DataLayer.Repository;
+public class ToDoRepository(ApplicationDbContext context,IMapper mapper):GenericRepository<ToDo>(context),IToDoRepository
+{
+    private readonly IMapper _mapper = mapper;
+    public async Task<ToDo?> UpdateAsync(int id, ToDoRequest request)
+    {
+        var todo = await GetByIdAsync(id);
+        if (todo == null)
+            return null;
+        _mapper.Map(request, todo);
+        return todo!;
+    }
+}
--- CatTask.Domain/IRepository/IToDoRepository.cs
+++ CatTask.Domain/IRepository/IToDoRepository.cs
@@ -1,6 +1,8 @@
-﻿using CatTask.Domain.Entities;
+﻿using CatTask.Domain.DTO.TODos;
+using CatTask.Domain.Entities;
 
 namespace CatTask.Domain.IRepository;
 public interface IToDoRepository:IGenericRepository<ToDo>
 {
+    Task<ToDo?> UpdateAsync(int id, ToDoRequest request);
 }
